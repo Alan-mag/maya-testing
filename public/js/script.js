3dccf1c8b119e55045302a743b672a36ea523a23
Alan Magelssen <alanmagelssen@gmail.com>
2016-04-27 01:18:52 -0700

    spanish hangout redirect

--- public/js/script.js
+++ public/js/script.js
@@ -36,3 +36,43 @@ var linko_3 = BASE_URL_3 + '?gid=95736247937&gd=INITIAL_DATA';
 console.log(linko_3);
 
 ///////////
+
+// ESPANOL //
+
+// button 1 info
+
+var BASE_URL_1_es = 'https://plus.google.com/hangouts/_/kxkmc3wyr5cbjn3c7klwiy4p6ye';
+var ssc_blob_1_es = '["",0,null,null,null,[],null,null,null,null,null,0,null,'
+  'null,null,[0],null,null,[],null,"0",null,null,null,null,null,null,'
+  'null,[],[],null,null,null,[],null,null,null,[],null,null,'
+  "[[\"#{803509868804}\",\"#{}\",2]]]";
+
+var linko_1_es = BASE_URL_1_es + '?gid=803509868804&gd=INITIAL_DATA';
+
+console.log(linko_1_es);
+
+/////////
+
+var BASE_URL_2_es = 'https://plus.google.com/hangouts/_/hso55bjh2jcyxpl6ptg5fah4jye';
+var ssc_blob_2_es = '["",0,null,null,null,[],null,null,null,null,null,0,null,'
+  'null,null,[0],null,null,[],null,"0",null,null,null,null,null,null,'
+  'null,[],[],null,null,null,[],null,null,null,[],null,null,'
+  "[[\"#{803509868804}\",\"#{}\",2]]]";
+
+var linko_2_es = BASE_URL_2_es + '?gid=803509868804&gd=INITIAL_DATA';
+
+console.log(linko_2_es);
+
+//////////
+
+var BASE_URL_3_es = 'https://plus.google.com/hangouts/_/kx3svexwrnftlj3aazuuky2mwue';
+var ssc_blob_3_es = '["",0,null,null,null,[],null,null,null,null,null,0,null,'
+  'null,null,[0],null,null,[],null,"0",null,null,null,null,null,null,'
+  'null,[],[],null,null,null,[],null,null,null,[],null,null,'
+  "[[\"#{803509868804}\",\"#{}\",2]]]";
+
+var linko_3_es = BASE_URL_3_es + '?gid=803509868804&gd=INITIAL_DATA';
+
+console.log(linko_3_es);
+
+///////////
